683994f1b5453b3e0126a05709814150eae0508c
danieldelahoz2212 <danielfranciscodelahoz@hotmail.com>
2022-10-15 19:27:16 -0500

    sea añadido tabla de lista

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { db } from '../firebase';
 import { collection, addDoc, deleteDoc, doc, onSnapshot } from 'firebase/firestore';
-import { async } from '@firebase/util';
 
 const Formulario = () => {
     const [nombre, setNombre] = useState('');
@@ -14,7 +13,7 @@ const Formulario = () => {
     const [listaPersona, setListaPersona] = useState([]);
     const getImage = async () => {
         try {
-            const res = await fetch('https://picsum.photos/200');
+            const res = await fetch('https://picsum.photos/300');
             const data = res.url;
             setFoto(data);
         } catch (error) {
@@ -83,14 +82,14 @@ const Formulario = () => {
         }
     }
 
-const editarInf = item => {
-    setNombre(item.nombreParticipante)
-    setCedula(item.numeroCedula)
-    setTelefono(item.numeroTelefono)
-    setCorreo(item.correoElectronico)
-    setDireccion(item.direccionVivienda)
-    setDescripcion(item.textoDescripcion)
-}
+    const editarInf = item => {
+        setNombre(item.nombreParticipante)
+        setCedula(item.numeroCedula)
+        setTelefono(item.numeroTelefono)
+        setCorreo(item.correoElectronico)
+        setDireccion(item.direccionVivienda)
+        setDescripcion(item.textoDescripcion)
+    }
 
     return (
         <div className='container mt-5'>
@@ -143,36 +142,83 @@ const editarInf = item => {
                         onChange={(e) => setDescripcion(e.target.value)}
                     />
 
-<img src={foto} alt="" className='mb-2'/>
+                    <img src={foto} alt="" className='mb-2' />
 
                     <button className='btn btn-primary btn-block' type='submit'>Agregar</button>
                     {/* </div> */}
 
                 </form>
             </div>
+            <hr className='my-4' />
             <div>
                 <h4 className='text-center'>Lista De Participantes</h4>
-                <ul className='list-group'>
-                    {
-                        listaPersona.map(item => (
-                            <li className='list-group-item' key={item.id}>
-                                <span className='lead'>
-                                    {item.nombreParticipante}
-                                    -{item.numeroCedula}
-                                    -{item.numeroTelefono}
-                                    -{item.correoElectronico}
-                                    -{item.direccionVivienda}
-                                    -{item.textoDescripcion}
-                                    <img src={item.foto} alt="" className='rounded-circle m-2' height={100} width={100} />
-                                </span>
-                                <button className='btn btn-danger btn-sm float-end mx-2' onClick={() => eliminar(item.id)}>Eliminar</button>
-                                <button className='btn btn-warning btn-sm float-end '>Editar</button>
-                            </li>
-                        ))
-                    }
-                </ul>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope='col'>Nombre Completo</th>
+                            <th scope='col'>Cedula</th>
+                            <th scope='col'>Telefono</th>
+                            <th scope='col'>Correo</th>
+                            <th scope='col'>Dirección</th>
+                            <th scope='col'>Descripcion</th>
+                            <th scope='col'>Imagen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            listaPersona.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.nombreParticipante}</td>
+                                    <td>{item.numeroCedula}</td>
+                                    <td>{item.numeroTelefono}</td>
+                                    <td>{item.correoElectronico}</td>
+                                    <td>{item.direccionVivienda}</td>
+                                    <td>{item.textoDescripcion}</td>
+                                    <td> <img src={item.foto} alt="" className='rounded-circle m-2' height={100} width={100} /></td>
+                                    <td className='col d-flex flex-column' >
+                                        <button className='btn btn-danger btn-sm float-end mb-2' onClick={() => eliminar(item.id)}>Eliminar</button>
+                                        <button className='btn btn-warning btn-sm float-end '>Editar</button>
+                                    </td>
+                                </tr>
+
+                            ))
+                        }
+                    </tbody>
+                </table>
+                {   /* listaPersona.map(item => (
+                <li className='list-group-item row' key={item.id}>
+
+                    <div className='justify-content-around d-flex'>
+                        <span>
+                            {item.nombreParticipante}
+                        </span>
+                        <td>
+                            {item.numeroCedula}
+                        </td>
+                        <td>
+                            {item.numeroTelefono}
+                        </td>
+                        <td>
+                            {item.correoElectronico}
+                        </td>
+                        <td>
+                            {item.direccionVivienda}
+                        </td>
+                        <td>
+                            {item.textoDescripcion}
+                        </td>
+                        <img src={item.foto} alt="" className='rounded-circle m-2' height={100} width={100} />
+                    </div>
+
+                    <div className='d-flex align-self-center justify-content-end my-2'>
+                        <button className='btn btn-danger btn-sm float-end mx-2' onClick={() => eliminar(item.id)}>Eliminar</button>
+                        <button className='btn btn-warning btn-sm float-end '>Editar</button>
+                    </div>
+                </li>
+                )) */
+                }
             </div>
-        </div>
+        </div >
     )
 }
 
